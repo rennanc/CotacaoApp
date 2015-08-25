213c8cf327220163f809d5cb4bc5590f6ffff51c
rennanc <rennanchagas@hotmail.com>
2015-08-24 23:53:12 -0300

    commit inicial

---
+++ CotacaoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CotacaoApp.Startup))]
+namespace CotacaoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
