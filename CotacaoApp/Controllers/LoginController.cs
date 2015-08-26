a6fc12b0877906676f43c9a2467a4c633fa0a2b7
rennanc <rennanchagas@hotmail.com>
2015-08-25 23:59:49 -0300

    pagina de login

---
+++ CotacaoApp/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using CotacaoApp.DAO;
+using CotacaoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CotacaoApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autenticar(string login, string senha)
+        {
+            UsuarioDAO usuarioDao = new UsuarioDAO();
+            Usuario usuario = usuarioDao.getByLogin(login, senha);
+            
+            if( usuario != null)
+            {
+                Session["UsuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Propostas");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ CotacaoApp/DAO/UsuarioDAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Common;
+using CotacaoApp.Models;
+using System.Web.UI.WebControls;
+
+namespace CotacaoApp.DAO
+{
+    public class UsuarioDAO
+    {
+
+        public void getById(int id)
+        {
+
+        }
+
+        public Usuario getByLogin(String usuario, String senha)
+        {
+
+            var session = new DBSession();
+            Query query = session.CreateQuery("select * from usuarios where username=@usuario and senha=@senha");
+            query.SetParameter("usuario", usuario);
+            query.SetParameter("senha", senha);
+            DbDataReader reader = query.ExecuteQuery();
+            Usuario user = null;
+            if (reader.Read())
+            {
+                user = new Usuario
+                {
+                    Login = reader.GetString(reader.GetOrdinal("username")),
+                    Senha = reader.GetString(reader.GetOrdinal("senha"))
+                };
+            }
+            return user;
+        }
+    }
+}
