cc1448e8093782ddeff93d86611ddec4ef84d3e4
rennanc <rennanchagas@hotmail.com>
2015-08-30 20:35:45 -0300

    arquivos faltantes do commit do crud de usuario

---
+++ CotacaoApp/DAO/DefaultConnection.cs
@@ -0,0 +1,20 @@
+﻿using CotacaoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CotacaoApp.DAO
+{
+    public class DefaultConnection : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
